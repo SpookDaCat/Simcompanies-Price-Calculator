2042ba7a9b39d2379e7e846aa6ef46fbb9a6d9e8
SpookDaCat <jmort593@uwsp.edu>
2023-04-17 11:11:13 -0500

    Add project files.

---
+++ Simcompanies Price Calculator/Dashboard.cs
@@ -0,0 +1,19 @@
+namespace Simcompanies_Price_Calculator {
+    public partial class Dashboard : Form {
+        public Dashboard() {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e) {
+
+        }
+    }
+}
