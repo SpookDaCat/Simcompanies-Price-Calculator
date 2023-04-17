33976014f565d29eb71720492c48ad0009ffd1a5
SpookDaCat <jmort593@uwsp.edu>
2023-04-17 17:04:26 -0500

    autocomplete working, price thing working. everything working

--- Simcompanies Price Calculator/Dashboard.Designer.cs
+++ Simcompanies Price Calculator/Dashboard.Designer.cs
@@ -25,6 +25,8 @@
         private void InitializeComponent() {
             this.label1 = new System.Windows.Forms.Label();
             this.QuickCalcGroupBox = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.MP25TextBox = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.MP3TextBox = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
@@ -32,9 +34,12 @@
             this.label3 = new System.Windows.Forms.Label();
             this.MPTextBox = new System.Windows.Forms.TextBox();
             this.ManualCalcGroupBox = new System.Windows.Forms.GroupBox();
-            this.PercentTextBox = new System.Windows.Forms.TextBox();
+            this.MCPercentTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.MCCostTextBox = new System.Windows.Forms.TextBox();
+            this.MPPercentTextBox = new System.Windows.Forms.TextBox();
             this.label9 = new System.Windows.Forms.Label();
-            this.PriceTextBox = new System.Windows.Forms.TextBox();
+            this.MPCostTextBox = new System.Windows.Forms.TextBox();
             this.CloseButton = new System.Windows.Forms.Button();
             this.InfoGroupBox = new System.Windows.Forms.GroupBox();
             this.label6 = new System.Windows.Forms.Label();
@@ -61,6 +66,8 @@
             // 
             // QuickCalcGroupBox
             // 
+            this.QuickCalcGroupBox.Controls.Add(this.label8);
+            this.QuickCalcGroupBox.Controls.Add(this.MP25TextBox);
             this.QuickCalcGroupBox.Controls.Add(this.label5);
             this.QuickCalcGroupBox.Controls.Add(this.MP3TextBox);
             this.QuickCalcGroupBox.Controls.Add(this.label4);
@@ -75,12 +82,33 @@
             this.QuickCalcGroupBox.Size = new System.Drawing.Size(333, 100);
             this.QuickCalcGroupBox.TabIndex = 3;
             this.QuickCalcGroupBox.TabStop = false;
-            this.QuickCalcGroupBox.Text = "Quick Values";
+            this.QuickCalcGroupBox.Text = "Quick Calculations";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(189, 42);
+            this.label8.Name = "label8";
+            this.label8.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label8.Size = new System.Drawing.Size(61, 15);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "-2.5% MP:";
+            // 
+            // MP25TextBox
+            // 
+            this.MP25TextBox.Location = new System.Drawing.Point(256, 42);
+            this.MP25TextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MP25TextBox.Name = "MP25TextBox";
+            this.MP25TextBox.ReadOnly = true;
+            this.MP25TextBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.MP25TextBox.Size = new System.Drawing.Size(71, 23);
+            this.MP25TextBox.TabIndex = 9;
+            this.MP25TextBox.Text = "$470.00";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(5, 70);
+            this.label5.Location = new System.Drawing.Point(189, 69);
             this.label5.Name = "label5";
             this.label5.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.label5.Size = new System.Drawing.Size(52, 15);
@@ -89,18 +117,19 @@
             // 
             // MP3TextBox
             // 
-            this.MP3TextBox.Location = new System.Drawing.Point(89, 68);
+            this.MP3TextBox.Location = new System.Drawing.Point(256, 69);
             this.MP3TextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MP3TextBox.Name = "MP3TextBox";
+            this.MP3TextBox.ReadOnly = true;
             this.MP3TextBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.MP3TextBox.Size = new System.Drawing.Size(89, 23);
+            this.MP3TextBox.Size = new System.Drawing.Size(71, 23);
             this.MP3TextBox.TabIndex = 7;
             this.MP3TextBox.Text = "$470.00";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(5, 44);
+            this.label4.Location = new System.Drawing.Point(189, 15);
             this.label4.Name = "label4";
             this.label4.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.label4.Size = new System.Drawing.Size(52, 15);
@@ -109,11 +138,12 @@
             // 
             // MP2TextBox
             // 
-            this.MP2TextBox.Location = new System.Drawing.Point(89, 42);
+            this.MP2TextBox.Location = new System.Drawing.Point(256, 15);
             this.MP2TextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MP2TextBox.Name = "MP2TextBox";
+            this.MP2TextBox.ReadOnly = true;
             this.MP2TextBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.MP2TextBox.Size = new System.Drawing.Size(89, 23);
+            this.MP2TextBox.Size = new System.Drawing.Size(71, 23);
             this.MP2TextBox.TabIndex = 7;
             this.MP2TextBox.Text = "$470.00";
             // 
@@ -134,34 +164,72 @@
             this.MPTextBox.Name = "MPTextBox";
             this.MPTextBox.ReadOnly = true;
             this.MPTextBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.MPTextBox.Size = new System.Drawing.Size(89, 23);
+            this.MPTextBox.Size = new System.Drawing.Size(94, 23);
             this.MPTextBox.TabIndex = 5;
             this.MPTextBox.Text = "$470.00";
             // 
             // ManualCalcGroupBox
             // 
-            this.ManualCalcGroupBox.Controls.Add(this.PercentTextBox);
+            this.ManualCalcGroupBox.Controls.Add(this.MCPercentTextBox);
+            this.ManualCalcGroupBox.Controls.Add(this.label7);
+            this.ManualCalcGroupBox.Controls.Add(this.MCCostTextBox);
+            this.ManualCalcGroupBox.Controls.Add(this.MPPercentTextBox);
             this.ManualCalcGroupBox.Controls.Add(this.label9);
-            this.ManualCalcGroupBox.Controls.Add(this.PriceTextBox);
+            this.ManualCalcGroupBox.Controls.Add(this.MPCostTextBox);
             this.ManualCalcGroupBox.Location = new System.Drawing.Point(9, 209);
             this.ManualCalcGroupBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ManualCalcGroupBox.Name = "ManualCalcGroupBox";
             this.ManualCalcGroupBox.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ManualCalcGroupBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.ManualCalcGroupBox.Size = new System.Drawing.Size(333, 45);
+            this.ManualCalcGroupBox.Size = new System.Drawing.Size(333, 77);
             this.ManualCalcGroupBox.TabIndex = 9;
             this.ManualCalcGroupBox.TabStop = false;
-            this.ManualCalcGroupBox.Text = "Manual Values";
+            this.ManualCalcGroupBox.Text = "Manual Calculations";
             // 
-            // PercentTextBox
+            // MCPercentTextBox
             // 
-            this.PercentTextBox.Location = new System.Drawing.Point(179, 15);
-            this.PercentTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.PercentTextBox.Name = "PercentTextBox";
-            this.PercentTextBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.PercentTextBox.Size = new System.Drawing.Size(72, 23);
-            this.PercentTextBox.TabIndex = 9;
-            this.PercentTextBox.Text = "3%";
+            this.MCPercentTextBox.Location = new System.Drawing.Point(179, 44);
+            this.MCPercentTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MCPercentTextBox.Name = "MCPercentTextBox";
+            this.MCPercentTextBox.ReadOnly = true;
+            this.MCPercentTextBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.MCPercentTextBox.Size = new System.Drawing.Size(72, 23);
+            this.MCPercentTextBox.TabIndex = 12;
+            this.MCPercentTextBox.Text = "3%";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(5, 46);
+            this.label7.Name = "label7";
+            this.label7.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label7.Size = new System.Drawing.Size(102, 15);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Calculate Percent:";
+            // 
+            // MCCostTextBox
+            // 
+            this.MCCostTextBox.Location = new System.Drawing.Point(256, 44);
+            this.MCCostTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MCCostTextBox.Name = "MCCostTextBox";
+            this.MCCostTextBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.MCCostTextBox.Size = new System.Drawing.Size(72, 23);
+            this.MCCostTextBox.TabIndex = 10;
+            this.MCCostTextBox.Text = "$470.00";
+            this.MCCostTextBox.TextChanged += new System.EventHandler(this.MCCostTextBox_TextChanged);
+            this.MCCostTextBox.Leave += new System.EventHandler(this.MCCostTextBox_Leave);
+            // 
+            // MPPercentTextBox
+            // 
+            this.MPPercentTextBox.Location = new System.Drawing.Point(179, 15);
+            this.MPPercentTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MPPercentTextBox.Name = "MPPercentTextBox";
+            this.MPPercentTextBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.MPPercentTextBox.Size = new System.Drawing.Size(72, 23);
+            this.MPPercentTextBox.TabIndex = 9;
+            this.MPPercentTextBox.Text = "3%";
+            this.MPPercentTextBox.TextChanged += new System.EventHandler(this.MPPercentTextBox_TextChanged);
+            this.MPPercentTextBox.Leave += new System.EventHandler(this.MPPercentTextBox_Leave);
             // 
             // label9
             // 
@@ -169,29 +237,31 @@
             this.label9.Location = new System.Drawing.Point(5, 17);
             this.label9.Name = "label9";
             this.label9.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.label9.Size = new System.Drawing.Size(159, 15);
+            this.label9.Size = new System.Drawing.Size(147, 15);
             this.label9.TabIndex = 6;
-            this.label9.Text = "Calculate discount (percent):";
+            this.label9.Text = "Calculate Custom Percent:";
             // 
-            // PriceTextBox
+            // MPCostTextBox
             // 
-            this.PriceTextBox.Location = new System.Drawing.Point(256, 15);
-            this.PriceTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.PriceTextBox.Name = "PriceTextBox";
-            this.PriceTextBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.PriceTextBox.Size = new System.Drawing.Size(72, 23);
-            this.PriceTextBox.TabIndex = 5;
-            this.PriceTextBox.Text = "$470.00";
+            this.MPCostTextBox.Location = new System.Drawing.Point(256, 15);
+            this.MPCostTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MPCostTextBox.Name = "MPCostTextBox";
+            this.MPCostTextBox.ReadOnly = true;
+            this.MPCostTextBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.MPCostTextBox.Size = new System.Drawing.Size(72, 23);
+            this.MPCostTextBox.TabIndex = 5;
+            this.MPCostTextBox.Text = "$470.00";
             // 
             // CloseButton
             // 
-            this.CloseButton.Location = new System.Drawing.Point(265, 258);
+            this.CloseButton.Location = new System.Drawing.Point(265, 297);
             this.CloseButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.CloseButton.Name = "CloseButton";
             this.CloseButton.Size = new System.Drawing.Size(77, 26);
             this.CloseButton.TabIndex = 10;
             this.CloseButton.Text = "Close";
             this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
             // 
             // InfoGroupBox
             // 
@@ -205,7 +275,7 @@
             this.InfoGroupBox.Size = new System.Drawing.Size(330, 76);
             this.InfoGroupBox.TabIndex = 11;
             this.InfoGroupBox.TabStop = false;
-            this.InfoGroupBox.Text = "Information";
+            this.InfoGroupBox.Text = "Basic Information";
             // 
             // label6
             // 
@@ -229,12 +299,15 @@
             // 
             // MarketNameTextBox
             // 
+            this.MarketNameTextBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.MarketNameTextBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
             this.MarketNameTextBox.Location = new System.Drawing.Point(135, 16);
             this.MarketNameTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MarketNameTextBox.Name = "MarketNameTextBox";
             this.MarketNameTextBox.Size = new System.Drawing.Size(189, 23);
             this.MarketNameTextBox.TabIndex = 15;
             this.MarketNameTextBox.Text = "On-Board Computer";
+            this.MarketNameTextBox.TextChanged += new System.EventHandler(this.MarketNameTextBox_TextChanged);
             // 
             // label2
             // 
@@ -251,6 +324,7 @@
             this.MarketIDTextBox.Location = new System.Drawing.Point(84, 16);
             this.MarketIDTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MarketIDTextBox.Name = "MarketIDTextBox";
+            this.MarketIDTextBox.ReadOnly = true;
             this.MarketIDTextBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.MarketIDTextBox.Size = new System.Drawing.Size(47, 23);
             this.MarketIDTextBox.TabIndex = 13;
@@ -258,19 +332,20 @@
             // 
             // RefreshButton
             // 
-            this.RefreshButton.Location = new System.Drawing.Point(9, 258);
+            this.RefreshButton.Location = new System.Drawing.Point(9, 297);
             this.RefreshButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.RefreshButton.Name = "RefreshButton";
             this.RefreshButton.Size = new System.Drawing.Size(77, 26);
             this.RefreshButton.TabIndex = 12;
             this.RefreshButton.Text = "&Refresh...";
             this.RefreshButton.UseVisualStyleBackColor = true;
+            this.RefreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
             // 
             // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(354, 297);
+            this.ClientSize = new System.Drawing.Size(354, 334);
             this.Controls.Add(this.RefreshButton);
             this.Controls.Add(this.InfoGroupBox);
             this.Controls.Add(this.CloseButton);
@@ -309,10 +384,15 @@
         public TextBox MPTextBox;
         public TextBox MP3TextBox;
         public TextBox MP2TextBox;
-        public TextBox PriceTextBox;
-        public TextBox PercentTextBox;
+        public TextBox MPCostTextBox;
+        public TextBox MPPercentTextBox;
         public TextBox QualityTextBox;
         public TextBox MarketNameTextBox;
         public TextBox MarketIDTextBox;
+        public TextBox MCPercentTextBox;
+        private Label label7;
+        public TextBox MCCostTextBox;
+        private Label label8;
+        public TextBox MP25TextBox;
     }
 }
--- Simcompanies Price Calculator/Dashboard.cs
+++ Simcompanies Price Calculator/Dashboard.cs
@@ -1,18 +1,25 @@
 using Simcompanies_Price_Calculator.Entities;
+using System.Globalization;
 using System.Linq;
 
 namespace Simcompanies_Price_Calculator {
     public partial class Dashboard : Form {
         private SimCoRestClient client = new SimCoRestClient();
         public List<MarketItem> Items { get; set; }
+        public Dictionary<int, string> MarketIndexer { get; set; }
+
+        public MarketItem CheapestItem { get; set; }
 
         public Dashboard() {
             InitializeComponent();
-            Items = client.GetMarketData(this.MarketIDTextBox.Text).OrderBy(x => x.Price).ToList();
-            decimal marketPrice = GetCheapestItem().Price;
-            this.MPTextBox.Text = marketPrice.ToString();
-            this.MP2TextBox.Text = Math.Round(marketPrice * 0.98m, 2).ToString();
-            this.MP3TextBox.Text = Math.Round(marketPrice * 0.97m, 2).ToString();
+
+            // generate market index & autocomplete
+            MarketIndexer = MarketIndex.GenerateIndex();
+            var autocomplete = new AutoCompleteStringCollection();
+            autocomplete.AddRange(MarketIndexer.Values.ToArray());
+            MarketNameTextBox.AutoCompleteCustomSource = autocomplete;
+
+            
         }
 
         private void Form1_Load(object sender, EventArgs e) {
@@ -24,5 +31,76 @@ namespace Simcompanies_Price_Calculator {
             return item ?? new MarketItem() { Price = -1};
         }
 
+        private void MarketNameTextBox_TextChanged(object sender, EventArgs e) {
+            var self = sender as TextBox;
+            if (self == null) return; // so it stops complaining
+
+            var t = MarketIndexer.FirstOrDefault(x=>x.Value == self.Text);
+            if (t.Value == null || t.Key == 0) {
+                // it was not found
+                return;
+            } else {
+                MarketIDTextBox.Text = t.Key.ToString();
+            }
+        }
+
+        public void RefreshMarketData() {
+            Items = client.GetMarketData(this.MarketIDTextBox.Text).OrderBy(x => x.Price).ToList();
+            CheapestItem = GetCheapestItem();
+            decimal marketPrice = CheapestItem.Price;
+            this.MPTextBox.Text = marketPrice.ToString("C", CultureInfo.CurrentCulture);
+            this.MP2TextBox.Text = Math.Round(marketPrice * 0.98m, 2).ToString("C", CultureInfo.CurrentCulture);
+            this.MP25TextBox.Text = Math.Round(marketPrice * 0.975m, 2).ToString("C", CultureInfo.CurrentCulture);
+            this.MP3TextBox.Text = Math.Round(marketPrice * 0.97m, 2).ToString("C", CultureInfo.CurrentCulture);
+
+            this.MPCostTextBox.Text = this.MPTextBox.Text;
+            this.MCCostTextBox.Text = this.MPTextBox.Text;
+        }
+
+        private void RefreshButton_Click(object sender, EventArgs e) {
+            RefreshMarketData();
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+
+        /// <summary>
+        /// MANUAL PERCENT: PERCENT TEXT BOX
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MPPercentTextBox_TextChanged(object sender, EventArgs e) {
+
+            var self = sender as TextBox;
+            if (self == null || !decimal.TryParse(self.Text.Replace("%",""), out decimal bigPercent)) {
+                return;
+            }
+            this.MPCostTextBox.Text = Math.Round(CheapestItem.Price * (100-bigPercent)/100, 2).ToString("C", CultureInfo.CurrentCulture);
+        }
+
+        /// <summary>
+        /// MANUAL COST: COST TEXT BOX
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MCCostTextBox_TextChanged(object sender, EventArgs e) {
+            var self = sender as TextBox;
+            if (self == null || !decimal.TryParse(self.Text.Replace("$",""), out decimal cost)) {
+                return;
+            }
+            var bigPercent = 100- (Math.Round((cost / CheapestItem.Price), 4) * 100);
+            this.MCPercentTextBox.Text = bigPercent.ToString() + "%";
+        }
+
+        private void MCCostTextBox_Leave(object sender, EventArgs e) {
+            var self = sender as TextBox;
+            if (!self.Text.StartsWith("$")) self.Text = "$"+self.Text;
+        }
+
+        private void MPPercentTextBox_Leave(object sender, EventArgs e) {
+            var self = sender as TextBox;
+            if (!self.Text.EndsWith("%")) self.Text = self.Text + "%";
+        }
     }
 }
